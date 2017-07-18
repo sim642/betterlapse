b4172840900c08e61088629aa74a68fca8daaeaf
Simmo Saan <simmo.saan@gmail.com>
2017-07-18 11:20:55 +0300

    Implement sliding frame averaging

--- main.cpp
+++ main.cpp
@@ -4,32 +4,68 @@
 #include <algorithm>
 #include <iterator>
 #include <opencv2/opencv.hpp>
+#include <cmath>
 
 using namespace std;
 namespace fs = boost::filesystem;
 
 int main()
 {
+    const double seconds = 15.0;
+    const double fps = 30.0;
+    const size_t frameCount = round(seconds * fps);
+
+    const fs::path imagesPath("./tl2/");
+    const fs::path framesPath("./frames/");
+    fs::create_directories(framesPath);
+
     vector<fs::path> imagePaths;
-    copy(fs::directory_iterator("./tl2/"), fs::directory_iterator(), back_inserter(imagePaths));
+    copy(fs::directory_iterator(imagesPath), fs::directory_iterator(), back_inserter(imagePaths));
     sort(imagePaths.begin(), imagePaths.end());
-
-    const cv::Size frameSize = cv::imread(imagePaths.front().string()).size();
-
-    cv::Mat sum(frameSize, CV_32SC3, cv::Scalar::all(0));
-
-    for (auto &p : imagePaths)
+    const size_t imageCount = imagePaths.size();
+    
+    vector<size_t> frameImages(frameCount);
+    for (size_t frame = 0; frame < frameCount; frame++)
     {
-        cout << p << endl;
-        cv::Mat image = cv::imread(p.string());
-        //sum += image;
-        cv::add(sum, image, sum, cv::noArray(), CV_32SC3);
+        frameImages[frame] = (static_cast<double>(imageCount - 1) / (frameCount - 1)) * frame;
     }
 
-    sum /= imagePaths.size();
-    cv::Mat avg;
-    sum.convertTo(avg, CV_8UC3);
-    cv::imwrite("./out.jpg", avg);
+    
+    const cv::Size size = cv::imread(imagePaths.front().string()).size();
+
+    cv::Mat prevSum(size, CV_32SC3, cv::Scalar::all(0));
+    size_t prevCount = 0;
+    for (size_t frameI = 0; frameI < frameCount; frameI++)
+    {
+        const size_t frameImage = frameImages[frameI];
+        const size_t nextFrameImage = frameI < frameCount - 1 ? frameImages[frameI + 1] : imageCount;
+
+        cout << "Frame " << frameI << ", Image: " << frameImage << endl;
+
+        cv::Mat image = cv::imread(imagePaths[frameImage].string());
+
+        cv::Mat nextSum(size, CV_32SC3, cv::Scalar::all(0));
+        size_t nextCount = 0;
+        for (size_t imageI = frameImage + 1; imageI < nextFrameImage; imageI++)
+        {
+            cout << "  " << "Next image: " << imageI << endl;
+            cv::Mat nextImage = cv::imread(imagePaths[imageI].string());
+            cv::add(nextSum, nextImage, nextSum, cv::noArray(), CV_32SC3);
+            nextCount++;
+        }
+
+        cv::Mat frameSum = prevSum;
+        cv::add(frameSum, image, frameSum, cv::noArray(), CV_32SC3);
+        frameSum += nextSum;
+
+        cv::Mat frame;
+        frameSum.convertTo(frame, CV_8UC3, 1.0 / (prevCount + 1 + nextCount));
+        fs::path framePath = framesPath / (to_string(frameI) + ".jpg");
+        cv::imwrite(framePath.string(), frame);
+
+        prevSum = nextSum;
+        prevCount = nextCount;
+    }
 
     return 0;
 }
